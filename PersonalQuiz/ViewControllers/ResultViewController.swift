984d57a5af06788d3a199ea761b71bf82572bfe6
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-04-08 15:45:26 +0500

    add algorithm result

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,12 +9,16 @@ import UIKit
 
 final class ResultViewController: UIViewController {
     
+    var result: Animal!
+    
     var answersChosen: [Answer]!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton.toggle()
-
+        
+       result = getResult(for: answersChosen)
+        
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
@@ -23,5 +27,33 @@ final class ResultViewController: UIViewController {
     
     deinit {
         print("\(type(of: self)) has been deallocated")
+    }
+}
+
+private extension ResultViewController {
+    func getResult(for answersChosen: [Answer]) -> Animal {
+        
+        var result: Animal
+        
+        var answerCats: [Answer]
+        var answerDogs: [Answer]
+        var answerRabbit: [Answer]
+        var answerTurtle: [Answer]
+        
+        answerCats = answersChosen.filter { $0.animal == .cat }
+        answerDogs = answersChosen.filter { $0.animal == .dog }
+        answerRabbit = answersChosen.filter { $0.animal == .rabbit }
+        answerTurtle = answersChosen.filter { $0.animal == .turtle }
+        
+        if answerCats.count <= answerDogs.count {
+            result = .dog
+        } else if answerDogs.count <= answerRabbit.count {
+            result = .rabbit
+        } else if answerRabbit.count <= answerTurtle.count {
+            result = .turtle
+        } else {
+            result = .cat
+        }
+        return result
     }
 }
